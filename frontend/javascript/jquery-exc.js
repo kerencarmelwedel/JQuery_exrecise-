26993c738d2f7d61b88042feecffc943ea1539fc
Keren <kerenwedel@gmail.com>
2019-01-30 00:35:13 +0200

    posts of random data

--- frontend/javascript/jquery-exc.js
+++ frontend/javascript/jquery-exc.js
@@ -1,41 +1,59 @@
-/*
-function User(id, name, username){
-	this.id = id;
-	this.name = name;
-	this.username= username;
+class Post {
+	constructor(id, title, poster, content) {
+		this.id = id;
+		this.title = title;
+		this.poster = poster;
+		this.content = content;
+ 		this.$el = null;
+	}
+	get title() {
+		return this._title;
+	}
+
+	set title(newTitle) {
+		this._title = newTitle;
+		this.toHTML().querySelector("#user-post-title").innerText = this.title;
+	}
+
+	get poster() {
+		return this._poster;
+	}
+
+	set poster(newPoster) {
+		this._poster = newPoster;
+		this.toHTML().querySelector("#poster").innerText = this.poster;
+	}
+
+	get content() {
+		return this._content;
+	}
+
+	set content(newContent) {
+		this._content = newContent;
+		this.toHTML().querySelector("#post-content").innerText = this.content;
+	}
+
+	toHTML() {
+		if (this.$el === null) {
+			//	this.$el.id = `post-${this.login.uuid}`;
+			//this.$el.querySelector(".user-img").src = this.picture.medium;
+			this.$el.querySelector("#user-post-title").innerText = this.title;
+			this.$el.querySelector("#poster").innerText = this.poster;
+			this.$el.querySelector("#post-content").innerText = this.content;
+			this.$el.querySelector("#user-post-id").innerText = this.id;
+			
+		}
+		return this.$el;
+	}
+
+	static create(rawPost) {
+		return post;
+	}
+
 }
-User.prototype.login = function() { this.isLoggedIn = true;}
-User.prototype.logout = function() { this.isLoggedIn = false;}
-
-const u = new User(1, "asaf blum", "asaf_blum")
-u.login()
-u.logout()
-u.isLoggedIn;
-
-u2.login()
-// undefined
-u2
-// User {id: 1, name: "asd", username: "zxczxc", isLoggedIn: true}
-u2.logout()
-// undefined
-u2
-// User {id: 1, name: "asd", username: "zxczxc", isLoggedIn: false}
-*/
-
-// DB structure -
-const post = {
-  id: 1,
-  title: "This is what happened to me today",
-  poster: "asaf",
-  content: "I went to the beach and then went out to get some lunch"
+const preparePostContainer = post => {
+	const $post = post.toHTML();
+	$postContainer.prepend($post); // Inject post element to dom
 };
-const user = { id: 1, user_name: "asaf_blum", full_name: "Asaf Blum" };
-const comment = { id: 1, content: "So cool!", post_id: 1 };
-const db = { post, comment, user };
-JSON.stringify(db);
-//"{"post":{"id":1,"title":"This is what happened to me today","poster":"asaf","content":"I went to the beach and then went out to get some lunch"},"comment":{"id":1,"content":"So cool!","post_id":1},"user":{"id":1,"user_name":"asaf_blum","full_name":"Asaf Blum"}}"
 
-// TODO: use https://www.generatedata.com/ to populate according to the above structure
-// TODO: fetch the data from the database, using json server
-// TODO: Create sortable(html hierarchy) for each db table - a container for each
-// TODO: Apply jQueryUI sortable
+ 
